f5df9785a5b480b88738265a7db39f1e83d6fb24
usmanabbas2211 <usmanabbas2211@gmail.com>
2023-09-21 11:00:22 +0500

    added react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import "./App.scss";
+import { Button } from "react-bootstrap";
+import CustomModal from "./components/Modal";
 
 function App() {
+  const [showModalA, setShowModalA] = useState(false);
+  const [showModalB, setShowModalB] = useState(false);
   return (
     <div className="App">
-      <button type="button" className="btn btn-outline-primary">
-        Button A
-      </button>
-      <button type="button" className="btn btn-outline-secondary">
-        Button B
-      </button>
+      <Button variant="outline-primary" onClick={() => setShowModalA(true)}>
+        Primary
+      </Button>{" "}
+      <Button variant="outline-secondary" onClick={() => setShowModalB(true)}>
+        Secondary
+      </Button>{" "}
+      <CustomModal show={showModalA} onHide={() => setShowModalA(false)} />
+      <CustomModal show={showModalB} onHide={() => setShowModalB(false)} />
     </div>
   );
 }
---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+interface ICustomModal {
+  show: boolean;
+  onHide: any;
+}
+
+function CustomModal({ show, onHide }: ICustomModal) {
+  const [modalShow, setModalShow] = useState(false);
+  return (
+    <Modal
+      show={show}
+      onHide={onHide}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default CustomModal;
